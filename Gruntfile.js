872d8032f0982e29a09fbc3cafcf7f194d88ad44
Roy Hewitt <royhewitt07@gmail.com>
2015-03-04 16:30:13 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+/* globals module, require */
+
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '* <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+        '* Source Files: <%= pkg.repository.url %> */\n'
+      },
+      global: {
+        files: [{
+          expand: true,
+          cwd: 'src/js',
+          src: '**/*.js',
+          dest: 'js'
+        }]
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: true,
+        force: true,
+        reporter: require('jshint-stylish')
+      },
+      all: ['src/js/*.js']
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          '_src/css/styles.css': '_src/scss/styles.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      global: {
+        src: 'src/css/styles.css',
+        dest: 'css/styles.css'
+      }
+    },
+
+    shell: {
+      jekyllServe: {
+        command: 'jekyll serve --baseurl='
+      },
+      jekyllBuild: {
+        command: 'jekyll build'
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      site: {
+        files: ['*.html', '**/*.html', '*.md', '**/*.md', '!_site/**/*.html', '!_site/**/*.md'],
+        tasks: ['shell:jekyllBuild']
+      },
+      js: {
+        files: ['src/js/*.js'],
+        tasks: ['jshint', 'shell:jekyllBuild']
+      },
+      css: {
+        files: ['src/scss/*.scss'],
+        tasks: ['sass', 'autoprefixer', 'shell:jekyllBuild']
+      },
+      svgIcons: {
+        files: ['svg/*.svg'],
+        tasks: ['svgstore', 'shell:jekyllBuild']
+      }
+    },
+
+    svgstore: {
+      options: {
+        prefix : 'shape-',
+        cleanup: false,
+        svg: {
+          style: 'display: none;'
+        }
+      },
+      default: {
+        files: {
+          '_includes/svg-defs.svg': ['svg/*.svg']
+        }
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('serve', ['shell:jekyllServe']);
+  grunt.registerTask('default', ['sass', 'jshint', 'watch']);
+  grunt.registerTask('build', ['sass', 'autoprefixer', 'jshint', 'uglify', 'shell:jekyllBuild']);
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+To get up and running you need to have two terminal windows open.
+
+1. In the first window type `grunt serve`, press enter and leave it alone.
+2. In the second window type `grunt`, press enter and leave it alone.
