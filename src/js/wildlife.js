ba5235a4cd3cf32fa2f0f0aaf08a660e3ff35dc6
Roy Hewitt <royhewitt07@gmail.com>
2015-03-11 12:45:07 -0400

    Refactored carousels

--- src/js/wildlife.js
+++ src/js/wildlife.js
@@ -1,51 +1,32 @@
 (function() {
   'use strict';
 
-  var $states = $('.state-carousel'),
-    $taxa = $('.taxa-carousel');
-
-    $taxa.slick({
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+  var options = {
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
         }
-      ]
-    });
-
-    $states.slick({
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
-      ]
-    });
+      }
+    ]
+  };
+
+  $('.carousel').each(function() {
+    $(this).slick(options);
+  });
+
+    
 })();
