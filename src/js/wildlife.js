7cdb24c93340fa9846d61705b076e8bbc5dc3c67
Roy Hewitt <royhewitt07@gmail.com>
2015-03-11 11:20:50 -0400

    Finished carousel mock-up

---
+++ src/js/wildlife.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  var $states = $('.state-carousel'),
+    $taxa = $('.taxa-carousel');
+
+    $taxa.slick({
+      centerMode: true,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    $states.slick({
+      centerMode: true,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+})();
