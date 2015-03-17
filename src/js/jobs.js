9ae27f598cc5ec1e956f05a5cf85a4d3e305af31
Roy Hewitt <royhewitt07@gmail.com>
2015-03-17 12:54:00 -0400

    Added jobs page

---
+++ src/js/jobs.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  var source = $('#jobs-template').html(),
+      template = Handlebars.compile(source),
+      $jobs = $('.job-list');
+
+  // Call the USA Jobs API
+  function sendRequest () {
+    var baseUrl = 'https://data.usajobs.gov/api/jobs',
+        parameters = {
+          OrganizationID: 'IN15',// Get results for USFWS only
+          NumberOfJobs: '250',
+          // Get results from R4 States/Provinces only
+          CountrySubDivision: 'Alabama;Arkansas;Florida;Georgia;Kentucky;Mississippi;North Carolina;Puerto Rico;South Carolina;Tennessee;Virgin Islands'
+        };
+
+    $.ajax({
+      url: baseUrl,
+      data: parameters
+    }).success( function (data) {
+      if (data.TotalJobs === 0) {
+        $jobs.html('Sorry, no results found.  Check back soon!');
+      } else {
+        $jobs.html( template(data) );
+      }
+    });
+  }
+
+  sendRequest();
+})();
